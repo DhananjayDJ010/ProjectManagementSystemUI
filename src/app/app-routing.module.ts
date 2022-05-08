7e8a225d0238b09a8c0958f535f53345aa5cf148
DhananjayDJ010 <dhananjayj010@gmail.com>
2022-05-08 19:51:54 +0530

    Added project management of user access

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 import { RegistrationLoginComponent } from './components/registration-login/registration-login.component';
 
 const routes: Routes = [
-  {'path': 'hello', component: RegistrationLoginComponent}
+  {'path': 'hello', component: RegistrationLoginComponent},
+  {'path': 'home', component: HomeComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import {SelectButtonModule} from 'primeng/selectbutton';
-import {PasswordModule} from 'primeng/password';
-import {ButtonModule} from 'primeng/button';
-import {RadioButtonModule} from 'primeng/radiobutton';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { AccordionModule } from 'primeng/accordion';
+import { TableModule } from 'primeng/table';
+import { DialogModule } from 'primeng/dialog';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextModule } from 'primeng/inputtext';
+import { MessagesModule } from 'primeng/messages';
+import { MessageModule } from 'primeng/message';
 import { RegistrationLoginComponent } from './components/registration-login/registration-login.component';
 import { FormsModule } from '@angular/forms';
 import RegistrationService from 'src/service/registration.service';
+import { AppHttpInterceptor } from 'src/service/interceptor.service';
+import { HomeComponent } from './components/home/home.component';
+import HomePageService from 'src/service/homepage.service';
+import ManageUserService from 'src/service/manage.users.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegistrationLoginComponent
+    RegistrationLoginComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
@@ -28,10 +40,20 @@ import RegistrationService from 'src/service/registration.service';
     FormsModule,
     BrowserAnimationsModule,
     RadioButtonModule,
-    HttpClientModule
+    HttpClientModule,
+    AccordionModule,
+    TableModule,
+    DialogModule,
+    DropdownModule,
+    MessagesModule,
+    MessageModule,
+    InputTextModule
   ],
   providers: [
-    RegistrationService
+    RegistrationService,
+    HomePageService,
+    ManageUserService,
+    { provide: HTTP_INTERCEPTORS, useClass: AppHttpInterceptor, multi: true }
   ],
   bootstrap: [AppComponent]
 })
