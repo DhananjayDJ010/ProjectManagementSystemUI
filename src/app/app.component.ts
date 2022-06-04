214e1957d4c802fac44c7334d8e498489e416602
DhananjayDJ010 <dhananjayj010@gmail.com>
2022-06-04 20:13:29 +0530

    change to make logout not appear in root

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NavigationEnd, Router} from '@angular/router';
+import { NavigationEnd, Router } from '@angular/router';
 import { MenuItem } from 'primeng/api';
 import LogoutService from 'src/service/logout.service';
 
@@ -10,23 +10,23 @@ import LogoutService from 'src/service/logout.service';
 })
 export class AppComponent implements OnInit {
   title = 'project-management-system';
-  state!:any;
+  state!: any;
   showMenuBar = true;
 
   ngOnInit() {}
 
-  constructor(private router: Router, private logout:LogoutService) {
+  constructor(private router: Router, private logout: LogoutService) {
     this.router.events.subscribe((event) => {
       this.state = null;
       if (event instanceof NavigationEnd) {
         this.state = event.url;
         console.log(this.state);
-        this.showMenuBar = this.state.includes('/hello');
+        this.showMenuBar = this.state.includes('/hello') || this.state === '/';
       }
     });
   }
 
-  logoutFunction(){
+  logoutFunction() {
     this.logout.logOut();
   }
 }
